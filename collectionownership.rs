4845e06a7fbca76d3355f177b84a8b6d86b0f983
tejaswinikurupati <137766792+tejaswinikurupati@users.noreply.github.com>
2023-06-28 14:53:59 +0530

    Create collectionownership.rs

---
+++ collectionownership.rs
@@ -0,0 +1,7 @@
+fn main(){
+use std::collections::HashMap;
+let mut scores = HashMap::new();
+scores.insert(string from ("Blue"),10);
+scores.insert(string from ("Blue"),25);
+println!("{:?}",scores);
+}

38cfb0e44a8278e2015c15ce6253944f2b3b398e
tejaswinikurupati <137766792+tejaswinikurupati@users.noreply.github.com>
2023-06-29 10:05:28 +0530

    Create var.rs

---
+++ var.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let int = 1;
+    let char = 'a';
+    let float = 10.00;
+    let string = "this is teju";
+    let bool = true;
+    println!("the value of int:{}",int);
+    println!("the value of char:{}",char);
+    println!("the value of float:{}",float);
+    println!("the value of string:{}",string);
+    println!("the value of bool:{}",bool);
+
+}

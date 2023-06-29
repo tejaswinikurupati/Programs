98144284883d114eea62b24703892e68d0a100b0
tejaswinikurupati <137766792+tejaswinikurupati@users.noreply.github.com>
2023-06-29 09:56:56 +0530

    Create structrectangle.rs

---
+++ structrectangle.rs
@@ -0,0 +1,9 @@
+fn main (){
+    let width1=30;
+    let height1=50;
+    println!("the area of rectangle is {} square pixels.",area(width1,height1));
+}
+fn area(width1:u32,height1:u32) ->u32{
+    width1*height1
+
+}
